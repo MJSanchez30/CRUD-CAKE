8fbab11efc9e52c6985ec8ddd5c43839221dc952
María José Sánchez <120137503+MJSanchez30@users.noreply.github.com>
2022-12-13 01:31:26 -0600

    Add files via upload

---
+++ templates/Producto/add.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \Cake\Datasource\EntityInterface $producto
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Producto'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="producto form content">
+            <?= $this->Form->create($producto) ?>
+            <fieldset>
+                <legend><?= __('Add Producto') ?></legend>
+                <?php
+                    echo $this->Form->control('nombre');
+                    echo $this->Form->control('precio');
+                    echo $this->Form->control('descricion');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
